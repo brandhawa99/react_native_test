00cbdebc15e9424ea45cd918a55a9d8a7e2b31b7
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-14 15:14:58 -0700

    initial

---
+++ type-app/src/screens/RecordMessage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const RecordMessage = (): JSX.Element =>{
+
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+
+export default RecordMessage;
