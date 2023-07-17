0d1db8b4fcce5c57c3c9d6011c5a0fda88502246
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-16 21:28:31 -0700

    initialed record message component

--- type-app/src/screens/RecordMessage.tsx
+++ type-app/src/screens/RecordMessage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { Audio } from 'expo-av';
 
 const RecordMessage = (): JSX.Element =>{
 
   return (
     <View>
 
+      
+      
     </View>
   )
 }
