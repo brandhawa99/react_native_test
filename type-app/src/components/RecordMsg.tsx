c7d2f0033f7965ca12e90d2f84a723717719c3ca
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-18 21:18:56 -0700

    voice message component

---
+++ type-app/src/components/RecordMsg.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import RecordButtons from './RecordButtons';
+
+const RecordMsg = ({recordFunc, playFunc, stopFunc, isRecording}:any) =>{
+  
+  return(
+    <View>
+      
+        <View style={styles.buttonsContainer}>
+          {
+            isRecording ?
+              <RecordButtons text="Stop Recording" press={stopFunc} >
+                <Entypo name="controller-stop" size={40}/>
+              </RecordButtons>
+              :
+              <RecordButtons text={"Start Recording"} press={recordFunc} buttonColor='#FB6767'>
+                <Entypo name='controller-record' size={40}/>
+              </RecordButtons>
+          }
+          <RecordButtons text="Play Recording" press={playFunc} buttonColor='#90EE90'>
+            <Entypo  name="controller-play" size={40}/>
+          </RecordButtons>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttons:{
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  buttonsContainer:{
+    flexDirection:"row",
+    justifyContent:"center",
+
+  }
+})
+
+export default RecordMsg;
