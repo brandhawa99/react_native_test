0e4d78cc13cbe94c4f81a7842aa6c5bc9b5ec23c
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-18 21:17:34 -0700

    crearted last page of userInputs

---
+++ type-app/src/components/ClosingPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+
+
+const ClosingPage = ({username}:any) =>{
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Your message has been saved {username}. The couple thanks you for your wonderful message ❤️ You are done!</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:"center",
+    paddingHorizontal:10,
+    backgroundColor:"lightgreen"
+  },
+  text:{
+    fontSize:30,
+    fontWeight:'400',
+    lineHeight:35,
+    textAlign:"center"
+  }
+})
+export default ClosingPage
