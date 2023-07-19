674ac235ea7a8b06b3e30f13731877c3ea712870
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-18 21:17:09 -0700

    added styles to navigator

--- type-app/App.tsx
+++ type-app/App.tsx
@@ -6,13 +6,28 @@ import { NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import {Ionicons,MaterialCommunityIcons, AntDesign, Entypo } from '@expo/vector-icons';
 import Recordings from './src/screens/Recordings';
+import { StatusBar } from 'react-native';
 const Tab = createBottomTabNavigator();
 export default function App() {
   useKeepAwake();
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveBackgroundColor:"white",
+        tabBarInactiveBackgroundColor:"black",
+        tabBarStyle:{
+          backgroundColor:'black'
+        },
+        headerStyle:{
+          backgroundColor:"black",
+        },
+        headerTitleStyle:{
+          color:"white",
+          fontSize:25,
+          fontWeight:"bold",
+        }
+      }}>
         <Tab.Screen 
           name="Record" 
           component={UserInputs}
@@ -40,7 +55,6 @@ export default function App() {
             tabBarIcon:({focused}) => focused ?
             <Entypo name="folder" size={24} color="black" />:
             <AntDesign name="folder1" size={24} color="black" />
-
           }}
         />
         </Tab.Navigator>
