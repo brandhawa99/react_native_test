2acb089fe09cde71061372380c42256eeb947253
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-18 21:18:35 -0700

    buttons component

---
+++ type-app/src/components/RecordButtons.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Pressable, Text, StyleSheet } from 'react-native';
+
+type RecordButtonsProps = {
+  text:string,
+  children?:JSX.Element,
+  press: ()=> void,
+  buttonColor?:string
+
+}
+const RecordButtons = ({text, press, buttonColor, children}:RecordButtonsProps) =>{
+
+  return(
+    <Pressable style={[styles.container,{backgroundColor:buttonColor||"beige"}]} onPress={press}>
+      {children}
+      <Text style={styles.text}>{text}</Text>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    justifyContent:"center",
+    alignItems:"center",
+    marginHorizontal:10,
+    paddingHorizontal:15,
+    paddingVertical:10,
+    borderRadius:15,
+    borderWidth:3,
+  },
+  text:{
+    fontSize:21,
+    fontWeight:"400"
+  }
+})
+
+export default RecordButtons
