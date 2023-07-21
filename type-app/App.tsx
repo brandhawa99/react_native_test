7c3d5cc9c91ed80fc1847de23ee02247688e6da6
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-21 12:16:14 -0700

    removed unused components

--- type-app/App.tsx
+++ type-app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState} from 'react';
 import {useKeepAwake} from 'expo-keep-awake'
 import UserInputs from './src/screens/UserInputs';
 import Settings from './src/screens/Settings';
@@ -6,46 +6,48 @@ import { NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import {Ionicons,MaterialCommunityIcons, AntDesign, Entypo } from '@expo/vector-icons';
 import Recordings from './src/screens/Recordings';
-import { StatusBar } from 'react-native';
+
 const Tab = createBottomTabNavigator();
+
 export default function App() {
   useKeepAwake();
+  
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarActiveBackgroundColor:"white",
-        tabBarInactiveBackgroundColor:"black",
-        tabBarStyle:{
-          backgroundColor:'black'
-        },
-        headerStyle:{
-          backgroundColor:"black",
-        },
-        headerTitleStyle:{
-          color:"white",
-          fontSize:25,
-          fontWeight:"bold",
-        }
-      }}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{
+          tabBarActiveBackgroundColor:"white",
+          tabBarInactiveBackgroundColor:"black",
+          tabBarStyle:{
+            backgroundColor:'black'
+          },
+          headerStyle:{
+            backgroundColor:"black",
+          },
+          headerTitleStyle:{
+            color:"white",
+            fontSize:25,
+            fontWeight:"bold",
+          }
+        }}>
         <Tab.Screen 
           name="Record" 
           component={UserInputs}
           options={{
             tabBarIcon: 
-              ({focused})=> !focused ? 
+            ({focused})=> !focused ? 
               <MaterialCommunityIcons name="record-circle-outline" size={24} color="black" />:
               <MaterialCommunityIcons name="record-circle" size={24} color="black" />
-          }}
-        />
+            }}
+            />
         <Tab.Screen 
           name="Settings" 
           component={Settings}
           options={{
             tabBarIcon: 
-              ({focused}) => focused ? 
-              <Ionicons name="settings" size={24} color="black" />:
-              <Ionicons name="settings-outline" size={24} color="black" />
+            ({focused}) => focused ? 
+            <Ionicons name="settings" size={24} color="black" />:
+            <Ionicons name="settings-outline" size={24} color="black" />
           }}
         />
         <Tab.Screen 
@@ -58,7 +60,7 @@ export default function App() {
           }}
         />
         </Tab.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
   );
 }
 
--- type-app/src/screens/Recordings.tsx
+++ type-app/src/screens/Recordings.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 
 const Recordings = () =>{
