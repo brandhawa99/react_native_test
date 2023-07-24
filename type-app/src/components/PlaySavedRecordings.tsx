d62789a82eac2993f935e7d0072d4c647fea46a6
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-24 13:31:45 -0700

    play saved folder

---
+++ type-app/src/components/PlaySavedRecordings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text } from 'react-native';
+
+
+const PlaySavedRecordings = ({fileName, fileLoc,}:any) =>{
+
+  return(
+    <View>
+      <Text></Text>
+
+    </View>
+  )
+}
