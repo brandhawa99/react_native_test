049de369717df1b524754386b18ec73058a4bcca
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-26 23:20:32 -0700

    updated nav buttons

--- type-app/src/components/NavigationButtons.tsx
+++ type-app/src/components/NavigationButtons.tsx
@@ -5,20 +5,69 @@ import { Ionicons } from '@expo/vector-icons';
 type NavButtonsProps = {
   next: () => void;
   prev: () => void;
+  pageNum: number;
+  reset:()=>void;
+  name:string;
 }
-export default function NavigationButtons({next ,prev}:NavButtonsProps){
-
-  return(
-    <View style={styles.container}>
-      <Pressable style={styles.textButton} onPress={prev}>
-        <Ionicons name="chevron-back" size={30} color="black" />
-      </Pressable>
+export default function NavigationButtons({next ,prev, pageNum, name, reset}:NavButtonsProps){
+  
+  switch(pageNum){
+    case 1: return(
+        <View style={styles.container}>
+          <Pressable style={styles.textButton}onPress={next}>
+            <Ionicons name="chevron-forward" size={30} color="black" /> 
+          </Pressable>
+        </View>
       
-      <Pressable style={styles.textButton}onPress={next}>
-        <Ionicons name="chevron-forward" size={30} color="black" /> 
-      </Pressable>
-    </View>
-  )
+    ) 
+    case 2:return(
+        <View style={styles.container}>
+          <Pressable style={styles.textButton} onPress={prev}>
+            <Ionicons name="chevron-back" size={30} color="black" />
+          </Pressable>
+          {
+            name &&
+          <Pressable style={styles.textButton}onPress={next}>
+            <Ionicons name="chevron-forward" size={30} color="black" /> 
+          </Pressable>
+          }
+        </View>
+      
+    )
+    case 3:return(
+
+        <View style={styles.container}>
+          <Pressable style={styles.textButton}onPress={prev}>
+            <Ionicons name="chevron-back" size={30} color="black" /> 
+          </Pressable>
+        </View>
+      
+    )
+    case 4: return(
+      <View style={styles.container}>
+        <Pressable style={styles.textButton} 
+          onPress={() =>{
+            reset();
+            next();
+          }} >
+          <Text>Press To Start</Text>
+        </Pressable>
+      </View>
+    )
+    default:{
+      return(
+        <View style={styles.container}>
+          <Pressable style={styles.textButton} onPress={prev}>
+            <Ionicons name="chevron-back" size={30} color="black" />
+          </Pressable>
+          <Pressable style={styles.textButton}onPress={next}>
+            <Ionicons name="chevron-forward" size={30} color="black" /> 
+          </Pressable>
+        </View>
+      )
+      
+    }
+  }
   
 }
 
@@ -27,9 +76,10 @@ const styles = StyleSheet.create({
     flexDirection:"row",
     fontSize:20,
     marginVertical:50,
-    justifyContent:"space-between",
+    justifyContent:"center",
     backgroundColor:"#D5BDAF",
     paddingHorizontal:20,
+    gap:50,
   },
   textButton:{
     justifyContent:"center",
