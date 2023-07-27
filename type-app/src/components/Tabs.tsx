e6d066c34747cbb9809019afb1229eb5e884d8fa
brandhawa99 <baltej.randhawa0@gmail.com>
2023-07-27 10:46:35 -0700

    removed settings

--- type-app/src/components/Tabs.tsx
+++ type-app/src/components/Tabs.tsx
@@ -2,9 +2,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import UserInputs from '../screens/UserInputs';
-import Settings from '../screens/Settings';
 import Recordings from '../screens/Recordings';
-import {MaterialCommunityIcons, Ionicons, Entypo, AntDesign} from "@expo/vector-icons"
+import {MaterialCommunityIcons, Entypo, AntDesign} from "@expo/vector-icons"
 import DirContextProvider from './DIrContextProvider';
 
 const Tab = createBottomTabNavigator();
@@ -32,26 +31,16 @@ const Tabs = () =>{
               fontWeight:"bold",
             }
       }}>
-      <Tab.Screen 
+        <Tab.Screen 
           name="Record" 
           component={UserInputs}
           options={{
             tabBarIcon: 
             ({focused})=> !focused ? 
             <MaterialCommunityIcons name="record-circle-outline" size={24} color="black" />:
-            <MaterialCommunityIcons name="record-circle" size={24} color="black" />
-            }}
-            />
-            <Tab.Screen 
-            name="Settings" 
-            component={Settings}
-            options={{
-              tabBarIcon: 
-            ({focused}) => focused ? 
-            <Ionicons name="settings" size={24} color="black" />:
-            <Ionicons name="settings-outline" size={24} color="black" />
+            <MaterialCommunityIcons name="record-circle" size={24} color="black" />,
           }}
-          />
+        />
         <Tab.Screen 
           name="Files"
           component={Recordings}
